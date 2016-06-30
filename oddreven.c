42834c929d7bcd7f24eef0b5235f16ddb0113d98
gomathikittusamy <gomsanu4@gmail.com>
2016-06-30 09:16:18 +0530

    Create oddreven.c

---
+++ oddreven.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("%d number is even",n);
+}
+else
+{
+printf("%d number is odd",n);
+}
+getch();
+}
